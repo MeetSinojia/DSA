1e80acd3b206bdcc7a896b1500252d02483a559d
MeetSinojia <71940146+MeetSinojia@users.noreply.github.com>
2023-05-30 12:16:00 +0530

    Add files via upload

---
+++ square_modulos.cpp
@@ -0,0 +1,17 @@
+long long modularExponentiation(long long base, long long exponent, long long mod)
+{
+    long long result = 1;
+    base %= mod;
+
+    while (exponent > 0)
+    {
+        if (exponent & 1)
+        {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+
+    return result;
+}

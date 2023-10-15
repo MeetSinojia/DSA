dd0f26cac7d4a5ca95be487eb2af29a3bdc7975f
MeetSinojia <71940146+MeetSinojia@users.noreply.github.com>
2023-10-15 22:35:18 +0530

    Update next_greater_element.cpp

--- next_greater_element.cpp
+++ next_greater_element.cpp
@@ -4,13 +4,18 @@ vector<int> nextGreaterElement(const vector<int>& nums)
     vector<int> result(n, -1); 
     stack<int> st;
 
-    for (int i = 0; i < n; ++i) {
-        while (!st.empty() && nums[i] > nums[st.top()]) 
+    for(int i=n-1;i>=0;i--)
+    {
+        while(!st.empty() && nums[st.top()]<nums[i])
         {
-            result[st.top()] = nums[i];
             st.pop();
+        }
+        if(!st.empty())
+        {
+            result[i]=st.top();
         }
         st.push(i);
     }
+
     return result;
 }
